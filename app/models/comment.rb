1c767f3515dac849bdd1d805a0f15df16b14e816
peter omunyidde <pomunyiddde@gmail.com>
2019-08-12 21:25:30 +0000

    Mailing on comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
 
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
+
 
    RATINGS = {
     'one star': '1_star',
@@ -13,6 +15,10 @@ class Comment < ApplicationRecord
 
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+
+  def send_comment_email
+      NotificationMailer.comment_added(self).deliver_now
   end
 
 end
